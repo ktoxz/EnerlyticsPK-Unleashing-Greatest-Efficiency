536198484d1de5c496f3cb786d12784d49d103a0
Ktoxz <69800528+ktoxz@users.noreply.github.com>
2023-07-05 17:22:10 +0700

    add wsgi server for deployment

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from website import create_app
-
+from waitress import serve
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    serve(app, host='0.0.0.0', port=50100, threads=2)
 
 
